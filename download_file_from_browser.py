1d35dea9163545ca2fabff4b07b7c6242a704c3c
Bodan1992 <bodan1992@gmail.com>
2022-07-23 23:36:56 +0300

    fix some line

--- download_file_from_browser.py
+++ download_file_from_browser.py
@@ -25,27 +25,3 @@ time.sleep(1)
 
 assert os.path.getsize("tmp/sampleFile.jpeg") == 4096
 os.remove('tmp/sampleFile.jpeg')
-
-#
-#
-# import time
-#
-#
-#
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-#
-# options = webdriver.ChromeOptions()
-# prefs = {
-#     "download.default_directory": os.path.join(current_dir, 'tmp'),
-#     "download.prompt_for_download": False
-# }
-# options.add_experimental_option("prefs", prefs)
-#
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-#
-# browser.config.driver = driver
-# browser.config.hold_browser_open = True
-# browser.open('https://demoqa.com/upload-download')
-# browser.element("#downloadButton").click()
-# time.sleep(1)
-# assert os.path.getsize("tmp/sampleFile.jpeg") == 4096
